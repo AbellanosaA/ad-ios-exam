379e9cf5f37bb54a6a5a6416ba1743543fe205a5
AbellanosaA <88261351+AbellanosaA@users.noreply.github.com>
2021-08-02 15:02:40 +0800

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,32 +6,39 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    public function getAllTodos() {
-    	// $todos = Todo::paginate(10);
-    	
-    	$todo = Todo::paginate(2);
+    public function getTodo(Request $request) {
+        $query = Todo::query();
 
-    	return response($todo, 200);
+        if ($s = $request->input('s')) {
+            $query->whereRaw("isactive LIKE '%" . $s . "%'");                
+        }
+
+        if ($sort = $request->input('sort')) {
+            $query->orderBy('id', $sort);
+        }
+
+        $perPage = 1;
+        $page = $request->input('page', 1);
+        $total = $query->count();
+
+      $result = $query->offset(($page - 1) * $perPage)->limit($perPage)->get();
+
+      return response()->json([
+          'data' => $result,
+          'total' => $total,
+          'page' => $page,
+          'last_page' => ceil($total / $perPage)
+      ], 404);
     }
 
     public function createTodo(Request $request) {
        $todo=new Todo;
-       $todo->isactive=1;       
+       $todo->isactive= $request->isactive;       
        $todo->save();
 
 	    return response($todo, 200);
     }
-
-    public function getTodo($id) {
-      if (Todo::where('id', $id)->exists()) {
-        $todo = Todo::where('id', $id)->get();
-        return response($todo, 200);
-      } else {
-        return response()->json([
-          "message" => "Todo not found"
-        ], 404);
-      }
-    }
+  
 
     public function updateTodo(Request $request, $id) {
 	    if (Todo::where('id', $id)->exists()) {
